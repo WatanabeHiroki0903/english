94756794e554714d5f64cd10c2cd68f957dd239d
渡邉弘貴 <hi.watanabe@reas.jp>
2017-04-09 14:12:20 +0900

    templates_cをgit無視リストに設定

--- original/data/Smarty/templates_c/%%25^258^2589B04E%%footer.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php /* Smarty version 2.6.30, created on 2017-04-01 12:23:48
-         compiled from C:%5Cxampp%5Chtdocs%5Cdemo_site%5Coriginal%5Cdata%5CSmarty%5Ctemplates/footer.tpl */ ?>
-</div>
-</body>
-</html>
